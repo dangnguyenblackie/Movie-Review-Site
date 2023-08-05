3500bbf74bf4b2175761f3427d6ad6ccea71e28a
howard.nguyen <howard.nguyen@nbowree.com>
2023-08-05 15:28:31 +0700

    Add create new Review in ReviewService

--- src/main/java/com/dangnp/howardmovie/Service/ReviewService.java
+++ src/main/java/com/dangnp/howardmovie/Service/ReviewService.java
@@ -1,9 +1,12 @@
 package com.dangnp.howardmovie.Service;
 
+import com.dangnp.howardmovie.Model.Movie;
 import com.dangnp.howardmovie.Model.Review;
 import com.dangnp.howardmovie.Repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,6 +18,13 @@ public class ReviewService {
     MongoTemplate mongoTemplate;
 
     public Review createNewReview(String imdbId, String body){
+        Review review = reviewRepository.insert(new Review(body));
 
+        mongoTemplate.update(Movie.class)
+                .matching(Criteria.where("imdbId").is(imdbId))
+                .apply(new Update().push("reviewIds").value(review))
+                .first();
+
+        return review;
     }
 }

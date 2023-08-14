bbaf90941a9e4d77d7c03979248888f88636fd23
howard.nguyen <howard.nguyen@nbowree.com>
2023-08-14 15:39:55 +0700

    Add Dockerfile and Docker-compose file

--- .history/movie-app/vite.config_20230814004900.ts
+++ .history/movie-app/vite.config_20230814004900.ts
@@ -6,7 +6,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
-    port: 8000, 
+    port: 8000, // This is the port which we will use in docker
+    // Thanks @sergiomoura for the window fix
+    // add the next lines if you're using windows and hot reload doesn't work
      watch: {
        usePolling: true
      }
